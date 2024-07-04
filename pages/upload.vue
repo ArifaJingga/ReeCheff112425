e934dd3b85946f1bd96644fe41ca8e805be5af4e
ArifaJingga <163821391+ArifaJingga@users.noreply.github.com>
2024-07-04 22:59:14 +0800

    Update upload.vue

--- pages/upload.vue
+++ pages/upload.vue
@@ -24,19 +24,16 @@
 </template>
 
 <script>
-
+  
 export default {
   data() {
     return {
-      isEdit: false,
-      recipe: {
-        judul: '',
-        deskripsi: '',
-        urlGambar: '',
-        asal: '',
-        kategori: ''
-      },
-      fileGambar: null
+      judul: '',
+      deskripsi: '',
+      fileGambar: null,
+      urlGambar: '',
+      asal: '',
+      kategori: ''
     };
   },
   methods: {
@@ -52,16 +49,16 @@ export default {
           await fileRef.put(this.fileGambar);
 
           // Dapatkan URL gambar
-          this.recipe.urlGambar = await fileRef.getDownloadURL();
+          this.urlGambar = await fileRef.getDownloadURL();
         }
 
         // Simpan resep ke Firestore
         const docRef = await firestore.collection('resep').add({
-          Judul: this.recipe.judul,
-          Deskripsi: this.recipe.deskripsi,
-          "URL Gambar": this.recipe.urlGambar,
-          Asal: this.recipe.asal,
-          Kategori: this.recipe.kategori,
+          Judul: this.judul,
+          Deskripsi: this.deskripsi,
+          "URL Gambar": this.urlGambar,
+          Asal: this.asal,
+          Kategori: this.kategori,
           createdAt: firebase.firestore.FieldValue.serverTimestamp()
         });
 
@@ -78,19 +75,10 @@ export default {
         // Menampilkan pesan error
         alert("Terjadi kesalahan saat mengunggah resep.");
       }
-    },
-    resetForm() {
-      this.recipe = {
-        judul: '',
-        deskripsi: '',
-        urlGambar: '',
-        asal: '',
-        kategori: ''
-      };
-      this.fileGambar = null;
     }
   }
 };
+
 </script>
 
 <style scoped>
@@ -132,11 +120,6 @@ input, textarea {
   cursor: pointer;
   transition: background-color 0.3s;
 }
-
-.btn:hover {
-  background-color: #cc5200;
-}
-</style>
 
 .btn:hover {
   background-color: #cc5200;
