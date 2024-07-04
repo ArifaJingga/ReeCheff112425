88f9d23ba5802988c3f868abac585b52bf7e19e5
ArifaJingga <163821391+ArifaJingga@users.noreply.github.com>
2024-07-04 18:44:05 +0800

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,8 +1,10 @@
-
 // plugins/firebase.js
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getFirestore, serverTimestamp } from "firebase/firestore";
+import { getAuth, signInAnonymously } from "firebase/auth";
+import { defineNuxtPlugin } from "@nuxtjs/composition-api"; // Gunakan ini jika menggunakan Nuxt 3 atau Composition API di Nuxt 2
 
+// Konfigurasi Firebase Anda
 const firebaseConfig = {
     apiKey: "AIzaSyBIuPiqHUi2Makb9yVqBK2NK3zDeZZUpiQ",
     authDomain: "reecfeff.firebaseapp.com",
@@ -10,16 +12,23 @@ const firebaseConfig = {
     storageBucket: "reecfeff.appspot.com",
     messagingSenderId: "687779412300",
     appId: "1:687779412300:web:8b4545df77facae2f637e9",
-    measurementId: "G-66EBPC65XN"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+let auth;
+
+if (typeof window !== "undefined") {
+  auth = getAuth(app);
+  signInAnonymously(auth).catch((error) => {
+    console.error("Error signing in anonymously:", error);
+  });
 }
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
-  
-  const firestore = firebase.firestore();
-  
-  export { firebase, firestore };
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.provide("firestore", firestore);
+});
 
 
 
