58403f6282190eea17f4a1dbab27979419499b2a
ArifaJingga <163821391+ArifaJingga@users.noreply.github.com>
2024-07-05 00:01:43 +0800

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,7 +1,7 @@
+
 // plugins/firebase.js
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/storage';
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBIuPiqHUi2Makb9yVqBK2NK3zDeZZUpiQ",
@@ -11,16 +11,14 @@ const firebaseConfig = {
     messagingSenderId: "687779412300",
     appId: "1:687779412300:web:8b4545df77facae2f637e9",
     measurementId: "G-66EBPC65XN"
-};
-
-// Check if Firebase has already been initialized
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
 }
 
-const firestore = firebase.firestore();
-const storage = firebase.storage();
-
-export { firestore, storage };
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  
+  const firestore = firebase.firestore();
+  
+  export { firebase, firestore };
 
 
