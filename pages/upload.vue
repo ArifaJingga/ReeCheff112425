b2679276aa28a6085f5cc20e844d7aa3cad22fda
ArifaJingga <163821391+ArifaJingga@users.noreply.github.com>
2024-07-04 22:48:53 +0800

    Update upload.vue

--- pages/upload.vue
+++ pages/upload.vue
@@ -24,16 +24,37 @@
 </template>
 
 <script>
-  
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+// Inisialisasi Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'API_KEY',
+    authDomain: 'PROJECT_ID.firebaseapp.com',
+    projectId: 'PROJECT_ID',
+    storageBucket: 'PROJECT_ID.appspot.com',
+    messagingSenderId: 'SENDER_ID',
+    appId: 'APP_ID'
+  });
+}
+
+const firestore = firebase.firestore();
+const storage = firebase.storage();
+
 export default {
   data() {
     return {
-      judul: '',
-      deskripsi: '',
-      fileGambar: null,
-      urlGambar: '',
-      asal: '',
-      kategori: ''
+      isEdit: false,
+      recipe: {
+        judul: '',
+        deskripsi: '',
+        urlGambar: '',
+        asal: '',
+        kategori: ''
+      },
+      fileGambar: null
     };
   },
   methods: {
@@ -49,16 +70,16 @@ export default {
           await fileRef.put(this.fileGambar);
 
           // Dapatkan URL gambar
-          this.urlGambar = await fileRef.getDownloadURL();
+          this.recipe.urlGambar = await fileRef.getDownloadURL();
         }
 
         // Simpan resep ke Firestore
         const docRef = await firestore.collection('resep').add({
-          Judul: this.judul,
-          Deskripsi: this.deskripsi,
-          "URL Gambar": this.urlGambar,
-          Asal: this.asal,
-          Kategori: this.kategori,
+          Judul: this.recipe.judul,
+          Deskripsi: this.recipe.deskripsi,
+          "URL Gambar": this.recipe.urlGambar,
+          Asal: this.recipe.asal,
+          Kategori: this.recipe.kategori,
           createdAt: firebase.firestore.FieldValue.serverTimestamp()
         });
 
@@ -75,10 +96,19 @@ export default {
         // Menampilkan pesan error
         alert("Terjadi kesalahan saat mengunggah resep.");
       }
+    },
+    resetForm() {
+      this.recipe = {
+        judul: '',
+        deskripsi: '',
+        urlGambar: '',
+        asal: '',
+        kategori: ''
+      };
+      this.fileGambar = null;
     }
   }
 };
-
 </script>
 
 <style scoped>
